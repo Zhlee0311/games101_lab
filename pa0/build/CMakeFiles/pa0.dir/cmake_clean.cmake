0889a5d8924de638e234e7419ed40340da07b517
Zhlee <lz1907041@gmail.com>
2023-11-02 21:44:01 +0800

    Adding files

---
+++ pa0/build/CMakeFiles/pa0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pa0.dir/main.cpp.o"
+  "pa0.pdb"
+  "pa0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pa0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
