0014c724076e92382b5c20003017c8de2dccba12
lzh <lz1907041@gmail.com>
2024-05-08 23:40:18 +0800

    rewrite .gitignore and CMakeLists.txt

--- AS0/main.cpp
+++ AS0/main.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
-#include<cmath>
-#include<eigen3/Eigen/Core>
+#include <iostream>
+#include <cmath>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
 using namespace Eigen;
 using namespace std;
 
-int main() {
+int main()
+{
     double cos45 = cos(45.0 / 180.0 * M_PI);
     double sin45 = sin(45.0 / 180.0 * M_PI);
     double _sin45 = sin45 * (-1);
     Vector3d p(2, 1, 1);
     Matrix3d k;
     k << cos45, _sin45, 1,
-            sin45, cos45, 2,
-            0, 0, 1;
+        sin45, cos45, 2,
+        0, 0, 1;
     Vector2d result = (k * p).head(2);
-    cout<<result<<endl;
+    cout << result << endl;
     return 0;
 }
